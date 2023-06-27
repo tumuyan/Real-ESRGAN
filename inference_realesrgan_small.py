bc0d943b2dcc689287811488ab6f99334de9c91f
tumuyan <tumuyan@gmail.com>
2023-06-27 21:34:04 +0800

    print decode error info and continue in inference_realesrgan_small.py

--- inference_realesrgan_small.py
+++ inference_realesrgan_small.py
@@ -190,6 +190,10 @@ def main():
       # print('Testing', idx, imgname)
 
       img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+      if img is None:
+          print("[Fail]opencv read file err, skip:",path)
+          continue
+
       if len(img.shape) == 3 and img.shape[2] == 4:
           img_mode = 'RGBA'
       else:
